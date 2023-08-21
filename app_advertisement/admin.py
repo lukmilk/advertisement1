7d7c1becb53fb26339fc450af685b2732cf21396
lukmilk <lukanovamd@gmail.com>
2023-08-21 15:28:44 +0700

    д/з

--- app_advertisement/admin.py
+++ app_advertisement/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Advertisement
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "price", "created_date","updated_date", "auction"]
+    list_filter = ["created_at", "auction"]
+    actions = ["make_auction_as_false", "make_auction_as_true"]
+    fieldsets = (
+        (
+            "Общее",{
+                "fields": ("title", "description"),
+            }
+
+        ),
+        (
+            "Финансы",{
+                "fields":("price","auction"),
+                "classes":["collapse"],
+            }
+        ),
+    )
+
+    @admin.display(description="Убрать возможность торга")
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+    @admin.display(description="Добавить возможность торга")
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- app_advertisement/models.py
+++ app_advertisement/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -33,6 +35,23 @@ class Advertisement(models.Model):
         verbose_name="Дата редактирования"
     )
 
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description="Дата редактирования")
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: orange; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     class Meta:
         db_table = 'advertisement'
 
